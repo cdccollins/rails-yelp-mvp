2166798ea046feaaa861cfc6002aefbea8e56d55
Celia Collins <celia.collins@hotmail.com>
2018-02-21 13:54:29 +0000

    restaurant pages

--- Gemfile
+++ Gemfile
@@ -5,7 +5,13 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
-
+#Design Gems
+gem 'bootstrap-sass'
+gem 'font-awesome-sass'
+gem 'simple_form'
+gem 'autoprefixer-rails'
+gem 'jquery-rails'
+gem 'simple_form'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
 # Use sqlite3 as the database for Active Record
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,39 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number: 0123,
+    category:     'belgian'
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number: 1234,
+    category:     'italian'
+  },
+  {
+    name:         'Noize',
+    address:      'Fitzrovia',
+    phone_number: 2345,
+    category:     'japanese'
+  },
+  {
+    name:         'Won Kei',
+    address:      'China town',
+    phone_number: 3456,
+    category:     'chinese'
+  },
+  {
+    name:         'Zedel',
+    address:      'Piccadilly Circus',
+    phone_number: 4567,
+    category:     'french'
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
